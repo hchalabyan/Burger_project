93929f9b53c613f767cc90b0ccd25c4aecbde541
hripsime_chalabyan <o9285161@nwytg.net>
2019-06-26 13:53:29 +0400

    Updates

---
+++ precache-manifest.c06d222286944095e289a2d7e6bcca54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed008b49584f4fd4eef7072b2079fed",
+    "url": "/Burger_project/index.html"
+  },
+  {
+    "revision": "7b1bdb42a29ede31cde3",
+    "url": "/Burger_project/static/css/main.e2d3832d.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/Burger_project/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "7b1bdb42a29ede31cde3",
+    "url": "/Burger_project/static/js/main.7e5872af.chunk.js"
+  },
+  {
+    "revision": "19ab098fa4dd3d58b830",
+    "url": "/Burger_project/static/js/runtime~main.ace6d4b7.js"
+  }
+]);
